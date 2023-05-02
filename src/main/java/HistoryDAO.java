f5e80a5f6ba274b0f16f061dddadfaee128c7fc7
YesunPark <lynn08082@gmail.com>
2023-05-02 23:15:00 +0900

    Add: 데이터베이스 조회, 추가 테스트 완료

--- src/main/java/HistoryDAO.java
+++ src/main/java/HistoryDAO.java
@@ -1,40 +1,42 @@
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.PreparedStatement;
-import java.sql.ResultSet;
-import java.sql.SQLException;
+
+import java.sql.*;
 
 // 데이터베이스 접근 클래스 
 public class HistoryDAO {
-	private Connection conn;
-	private PreparedStatement pstmt;
-	private ResultSet rs;
+	private Connection conn = null;
+	private PreparedStatement pstmt = null;
+	private ResultSet rs = null;
 
 	public HistoryDAO() {
-		
+
 	}
 
-	public void select() {
-		
+	// 테스트용 메서드
+	public String selectTest() {
+		String dbURL = "jdbc:mariadb://localhost:3306/mission1";
+		String dbID = "mission1user";
+		String dbPW = "mission1";
+
 		try {
-			String dbURL = "jdbc:mariadb://localhost:3306/mission1";
-			String dbID = "mission1user";
-			String dbPW = "mission1";
 			Class.forName("org.mariadb.jdbc.Driver");
-			conn = DriverManager.getConnection(dbURL, dbID, dbPW);
-		} catch (Exception e) {
+		} catch (ClassNotFoundException e) {
 			e.printStackTrace();
 		}
-		String sql = "select * from location_history";
+
 		try {
+			conn = DriverManager.getConnection(dbURL, dbID, dbPW);
+
+			String sql = "select * from location_history";
+
 			pstmt = conn.prepareStatement(sql);
 			rs = pstmt.executeQuery();
-			
-			int affectedRows = pstmt.executeUpdate();
-			if (affectedRows > 0) {
-				System.out.println("select 성공!");
-			} else {
-				System.out.println("select 실패!");
+
+			while (rs.next()) {
+				String id = rs.getString("id");
+				String xCoordinate = rs.getString("x_coordinate");
+				String yCoordinate = rs.getString("y_coordinate");
+
+				return id + ", " + xCoordinate + ", " + yCoordinate;
 			}
 		} catch (SQLException e) {
 			e.printStackTrace();
@@ -46,6 +48,7 @@ public class HistoryDAO {
 			} catch (SQLException e) {
 				e.printStackTrace();
 			}
+
 			try {
 				if (pstmt != null && !pstmt.isClosed()) {
 					pstmt.close();
@@ -62,13 +65,78 @@ public class HistoryDAO {
 				e.printStackTrace();
 			}
 		}
+		return "데이터베이스 오류입니다!";
 	}
-	
-	 public static void main(String[] args) {
-		 HistoryDAO dbTest = new HistoryDAO();
-//	        dbTest.dbSelect();  // static 메소드가 아니라서 바로 실행은 불가능. 인스턴스화하면 가능
-//	        dbTest.dbInsert();
-//	        dbTest.dbUpdate();
-	        dbTest.select();
-	    }
+
+	// insert 테스트용 메서드 
+	public String insertTest() {
+		String dbURL = "jdbc:mariadb://localhost:3306/mission1";
+		String dbID = "mission1user";
+		String dbPW = "mission1";
+
+		try {
+			Class.forName("org.mariadb.jdbc.Driver");
+		} catch (ClassNotFoundException e) {
+			e.printStackTrace();
+		}
+
+		double xCoordinateValue = 4.11111;
+		double yCoordinateValue = 4.22222;
+		Date lookupDate = new Date(0);
+
+		try {
+			conn = DriverManager.getConnection(dbURL, dbID, dbPW);
+
+			String sql = "INSERT INTO location_history ( x_coordinate, y_coordinate, lookup_date )\n"
+					+ "VALUES ( ?, ?, ?)";
+
+			pstmt = conn.prepareStatement(sql);
+			pstmt.setDouble(1, xCoordinateValue);
+			pstmt.setDouble(2, yCoordinateValue);
+			pstmt.setDate(3, lookupDate);
+
+			int affectedRows = pstmt.executeUpdate();
+			if (affectedRows > 0) {
+				return "저장성공!";
+			} else {
+				return "저장실패!";
+			}
+		} catch (SQLException e) {
+			e.printStackTrace();
+		} finally {
+			try {
+				if (rs != null && !rs.isClosed()) {
+					rs.close();
+				}
+			} catch (SQLException e) {
+				e.printStackTrace();
+			}
+
+			try {
+				if (pstmt != null && !pstmt.isClosed()) {
+					pstmt.close();
+				}
+			} catch (SQLException e) {
+				e.printStackTrace();
+			}
+
+			try {
+				if (conn != null && !conn.isClosed()) {
+					conn.close();
+				}
+			} catch (SQLException e) {
+				e.printStackTrace();
+			}
+		}
+		return "데이터베이스 오류입니다!";
+	}
+
+	// 테스트용 메인 함수
+	public static void main(String[] args) {
+		HistoryDAO dbTest = new HistoryDAO();
+//		System.out.println(dbTest.selectTest());
+//		System.out.println(dbTest.insertTest());
+		
+
+	}
 }
