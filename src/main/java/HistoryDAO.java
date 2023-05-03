200221e2ac9caad361f1d4783434c6c7ee3f7751
YesunPark <lynn08082@gmail.com>
2023-05-03 18:16:37 +0900

    Add: 위치 히스토리 저장 메소드 추가(아직 위도, 경도 받아오는 로직은 구성 전)

--- src/main/java/HistoryDAO.java
+++ src/main/java/HistoryDAO.java
@@ -1,5 +1,9 @@
 
 import java.sql.*;
+import java.time.*;
+import java.time.format.DateTimeFormatter;
+
+
 
 // 데이터베이스 접근 클래스 
 public class HistoryDAO {
@@ -9,6 +13,70 @@ public class HistoryDAO {
 
 	public HistoryDAO() {
 
+	}
+
+	// 내가 조회한 위치 히스토리 저장
+	public int saveLocationHistory() {
+		String dbURL = "jdbc:mariadb://localhost:3306/mission1";
+		String dbID = "mission1user";
+		String dbPW = "mission1";
+
+		try {
+			Class.forName("org.mariadb.jdbc.Driver");
+		} catch (ClassNotFoundException e) {
+			e.printStackTrace();
+		}
+
+		double xCoordinateValue = 4.11111;
+		double yCoordinateValue = 4.22222;
+		String lookupDate = (LocalDateTime.now()).format(DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss"));
+
+		try {
+			conn = DriverManager.getConnection(dbURL, dbID, dbPW);
+
+			String sql = "INSERT INTO location_history ( x_coordinate, y_coordinate, lookup_date )\n"
+					+ "VALUES ( ?, ?, ?)";
+
+			pstmt = conn.prepareStatement(sql);
+			pstmt.setDouble(1, xCoordinateValue);
+			pstmt.setDouble(2, yCoordinateValue);
+			pstmt.setString(3, lookupDate);
+
+			int affectedRows = pstmt.executeUpdate();
+			if (affectedRows > 0) {
+				return 1; // 저장 성공 
+			} else {
+				return -1; // 저장 실패 
+			}
+		} catch (SQLException e) {
+			e.printStackTrace();
+		} finally {
+			try {
+				if (rs != null && !rs.isClosed()) {
+					rs.close();
+				}
+			} catch (SQLException e) {
+				e.printStackTrace();
+			}
+
+			try {
+				if (pstmt != null && !pstmt.isClosed()) {
+					pstmt.close();
+				}
+			} catch (SQLException e) {
+				e.printStackTrace();
+			}
+
+			try {
+				if (conn != null && !conn.isClosed()) {
+					conn.close();
+				}
+			} catch (SQLException e) {
+				e.printStackTrace();
+			}
+		}
+
+		return -2; // 데이터베이스 오류 
 	}
 
 	// 테스트용 메서드
@@ -68,7 +136,7 @@ public class HistoryDAO {
 		return "데이터베이스 오류입니다!";
 	}
 
-	// insert 테스트용 메서드 
+	// insert 테스트용 메서드
 	public String insertTest() {
 		String dbURL = "jdbc:mariadb://localhost:3306/mission1";
 		String dbID = "mission1user";
@@ -135,8 +203,8 @@ public class HistoryDAO {
 	public static void main(String[] args) {
 		HistoryDAO dbTest = new HistoryDAO();
 //		System.out.println(dbTest.selectTest());
+		System.out.println(dbTest.saveLocationHistory());
 //		System.out.println(dbTest.insertTest());
-		
 
 	}
 }
